3dfacf92d63287258d9d35952463c7428e76f873
why <why@why.com>
2023-05-31 19:01:54 +0800

    XPath的使用

---
+++ 3.1/demo18.py
@@ -0,0 +1,17 @@
+from lxml import etree
+
+html = etree.parse('./test.html', etree.HTMLParser())
+result = html.xpath('//li[1]/ancestor::*')
+print(result)
+result = html.xpath('//li[1]/ancestor::div')
+print(result)
+result = html.xpath('//li[1]/attribute::*')
+print(result)
+result = html.xpath('//li[1]/child::a[@href="link1.html"]')
+print(result)
+result = html.xpath('//li[1]/descendant::span')
+print(result)
+result = html.xpath('//li[1]/following::*[2]')
+print(result)
+result = html.xpath('//li[1]/following-sibling::*')
+print(result)
