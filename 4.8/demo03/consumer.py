7af1d38a4ef5af3ffd95ce6154f0eb375065c3f8
why <why@why.com>
2023-06-06 15:11:02 +0800

    RabbitMQ的优先级队列，增加’x-max-priority‘属性

---
+++ 4.8/demo03/consumer.py
@@ -0,0 +1,13 @@
+import pika
+
+MAX_PRIORITY = 100
+QUEUE_NAME = 'scrape'
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue=QUEUE_NAME, arguments={'x-max-priority': MAX_PRIORITY})
+
+while True:
+    input()
+    method_frame, header, body = channel.basic_get(queue=QUEUE_NAME, auto_ack=True)
+    if body:
+        print(f'Get {body}')
